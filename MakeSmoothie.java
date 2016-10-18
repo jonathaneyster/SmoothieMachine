464c4a55f8c882c42a84e12e7b08696077e2cdae
jonathaneyster <jonathaneyster@gmail.com>
2016-10-18 14:07:03 -0400

    step 5

--- MakeSmoothie.java
+++ MakeSmoothie.java
@@ -1,9 +1,6 @@
 package smoothieShoppe;
-
 import java.util.Scanner;
 import java.io.IOException;
-
-
 public class MakeSmoothie 
 {
 	public static void main(String[] args) throws IOException
@@ -11,22 +8,15 @@ public class MakeSmoothie
 		SmoothieRecipe myRecipe = new SmoothieRecipe();
 		Scanner in = new Scanner(System.in);
 		boolean loop = false;
-		
+	
 		while (loop == false)
 		{	
-			System.out.println("--Smoothie-Shoppe-------------------------------------------------------------");
-			System.out.println("Available actions: [b]rowse smoothies, [q]uit");
+			myRecipe.mainMenu();
 			System.out.print(">");
 			String s = in.next();
-			System.out.println("------------------------------------------------------------------------------");
 			if (s.equals("b"))
 			{
-				System.out.println("Recipes: ");
-				System.out.println();
-				myRecipe.printRecipes();
-				System.out.println();
-				System.out.println("------------------------------------------------------------------------------");
-				System.out.println("Available actions: [v]iew ingredients, [o]rder smoothie, [q]uit");
+				myRecipe.recipeMenu();
 				System.out.print(">");
 				s = in.next();
 				if (s.equals("q"))
@@ -36,115 +26,200 @@ public class MakeSmoothie
 				}
 				else if (s.equals("v"))
 				{	
-					System.out.println();
-					myRecipe.printIngredients();
-					System.out.println();
-					System.out.println();
-					System.out.println("Enter a number (1-3) to order the smoothie.");
+					myRecipe.ingredientsMenu();
 					System.out.print(">");
 					s = in.next();
 					if (s.equals("1"))
 					{	
-						myRecipe.Smoothie1();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();		
-						System.out.println("------------------------------------------------------------------------------");
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie1Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
-						}						
+						}
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+								
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}							
+						}
 					}
 					else if (s.equals("2"))
 					{	
-						myRecipe.Smoothie2();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();
-						System.out.println("------------------------------------------------------------------------------");					
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie2Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
+						}
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+								
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}							
 						}
 					}
 					else if (s.equals("3"))
 					{	
-						myRecipe.Smoothie3();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();
-						System.out.println("------------------------------------------------------------------------------");					
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie3Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
-						}						
+						}	
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+								
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}							
+						}
 					}				
 				}
 				else if (s.equals("o"))
 				{	
-					System.out.println();
-					System.out.println("Enter a number (1-3) to order the smoothie.");
+					myRecipe.orderMenu();
 					System.out.print(">");
 					s = in.next();
 					if (s.equals("1"))
 					{	
-						myRecipe.Smoothie1();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();		
-						System.out.println("------------------------------------------------------------------------------");
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie1Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
-						}						
+						}	
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+								
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}							
+						}
 					}
 					else if (s.equals("2"))
 					{	
-						myRecipe.Smoothie2();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();
-						System.out.println("------------------------------------------------------------------------------");					
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie2Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
+						}
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+							
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}
 						}
 					}
 					else if (s.equals("3"))
 					{	
-						myRecipe.Smoothie3();
-						myRecipe.showRecipe();
-						System.out.println();
-						myRecipe.printInventory();
-						System.out.println();
-						System.out.println("------------------------------------------------------------------------------");					
-						System.out.println("Order another smoothie? (y/n)");
+						myRecipe.smoothie3Menu();
 						System.out.print(">");
 						s = in.next();
 						if (s.equals("n"))
 						{
+							System.out.println("Goodbye.");
 							loop = true;
-						}						
+						}
+						if (s.equals("c"))
+						{
+							myRecipe.viewCart();
+							System.out.print(">");
+							s = in.next();
+							if (s.equals("m"))
+							{	
+								
+							}
+							else if (s.equals("c"))
+							{
+								myRecipe.showRecipe();
+								loop = true;
+							}
+							else if (s.equals("q"))
+							{
+								System.out.println("Goodbye.");
+								loop = true;
+							}							
+						}
 					}					
 				}
 			}
@@ -157,4 +232,3 @@ public class MakeSmoothie
 		in.close();
 	}
 }
-
--- SmoothieRecipe.java
+++ SmoothieRecipe.java
@@ -2,7 +2,6 @@ package smoothieShoppe;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,7 +13,8 @@ import Smoothie.Peach;
 
 public class SmoothieRecipe
 {
-List<Fruit> ingredients = new ArrayList<>();	
+	List<Fruit> ingredients = new ArrayList<>();
+	List<String> orderedSmoothies = new ArrayList<>();
 //Add Fruit to ArrayList	
 	public void addFruit(Fruit fruit)
 	{		
@@ -56,31 +56,124 @@ List<Fruit> ingredients = new ArrayList<>();
 //Create smoothies from recipe
 	public void Smoothie1()
 	{
+		int orderedSmoothie1=0;
+		orderedSmoothie1++;
+		
 		ingredients.add(new Apple());
 		ingredients.add(new Orange());
+		orderedSmoothies.add("Apple orange smoothie: " + orderedSmoothie1);
 
 	}
 	public void Smoothie2()
 	{
+		int orderedSmoothie2=0;
+		orderedSmoothie2++;
+		
 		ingredients.add(new Apple());
 		ingredients.add(new Peach());
+		orderedSmoothies.add("Apple peach smoothies: " + orderedSmoothie2);
 
 	}
 	public void Smoothie3()
 	{
+		int orderedSmoothie3=0;
+		orderedSmoothie3++;
+		
 		ingredients.add(new Orange());
 		ingredients.add(new Peach());
+		orderedSmoothies.add("Orange peach smoothies: " + orderedSmoothie3);
 	}
+//cart function
+	public void viewCart()
+	{
+		for (int i=0; i<orderedSmoothies.size(); i++)
+		{	
+			System.out.println(orderedSmoothies.get(i));
+		}
+		System.out.println("");
+		System.out.println("Available actions: [m]ain menu, [c]heckout, [q]uit");
+	}
+//main menu
+	public void mainMenu()
+	{
+		System.out.println("--Smoothie-Shoppe-------------------------------------------------------------");
+		System.out.println("Available actions: [b]rowse smoothies, [q]uit");
+	}
+//recipe menu
+	public void recipeMenu() throws FileNotFoundException
+	{
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println("Recipes: ");
+		System.out.println();
+		printRecipes();
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println("Available actions: [v]iew ingredients, [o]rder smoothie, [q]uit");		
+	}
+//ingredients menu
+	public void ingredientsMenu() throws FileNotFoundException
+	{
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------");
+		printIngredients();
+		System.out.println();
+		System.out.println();
+		System.out.println("Enter a number (1-3) to order the smoothie.");		
+	}
+//order menu
+	public void orderMenu()
+	{
+		System.out.println();
+		System.out.println("Enter a number (1-3) to order the smoothie.");
+	}
+	
+//smoothie1 menu
+	public void smoothie1Menu() throws FileNotFoundException
+	{
+		Smoothie1();
+		showRecipe();
+		System.out.println();
+		printInventory();
+		System.out.println();		
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println("Order another smoothie? (y/n)");
+		System.out.println("Available actions: [c]art");		
+	}
+//smoothie2 menu
+	public void smoothie2Menu() throws FileNotFoundException
+	{
+		Smoothie2();
+		showRecipe();
+		System.out.println();
+		printInventory();
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------");					
+		System.out.println("Order another smoothie? (y/n)");
+		System.out.println("Available actions: [c]art");
+	}
+//smoothie3 menu
+	public void smoothie3Menu() throws FileNotFoundException
+	{
+		Smoothie3();
+		showRecipe();
+		System.out.println();
+		printInventory();
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------");					
+		System.out.println("Order another smoothie? (y/n)");
+		System.out.println("Available actions: [c]art");		
+	}
+	
 //Return two ingredients, name, cost, and sum 		
-		public void showRecipe()
-		{
-			System.out.println("------------------------------------------------------------------------------");
-			System.out.print("The first ingredient of the smoothie is: " + ingredients.get(0).getInfo() + " ($" + ingredients.get(0).getCost() + ")");
-			System.out.println();
-			System.out.println();
-			System.out.print("The second ingredient of the smoothie is: "+ ingredients.get(1).getInfo() + " ($" + ingredients.get(1).getCost() + ")");
-			System.out.println();
-			System.out.println("------------------------------------------------------------------------------");
-			System.out.println("Total cost of your " + ingredients.get(0).getInfo() + " " + ingredients.get(1).getInfo() + " " + "smoothie is: $" + (ingredients.get(0).getCost()+ingredients.get(1).getCost()));
-		}	
+	public void showRecipe()
+	{
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.print("The first ingredient of the smoothie is: " + ingredients.get(0).getInfo() + " ($" + ingredients.get(0).getCost() + ")");
+		System.out.println();
+		System.out.println();
+		System.out.print("The second ingredient of the smoothie is: "+ ingredients.get(1).getInfo() + " ($" + ingredients.get(1).getCost() + ")");
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println("Total cost of your " + ingredients.get(0).getInfo() + " " + ingredients.get(1).getInfo() + " " + "smoothie is: $" + (ingredients.get(0).getCost()+ingredients.get(1).getCost()));
+	}	
 }
